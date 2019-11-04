d882ca325ed3975a121b9cd615d0e072bd46f42b
c00lmk <maciek.kasprzak@coolmaciek.pl>
2019-11-04 17:56:34 +0100

    Adds new Twig Extensions which enables to use route' names in twig templates via new route() function.

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 
+use App\Views\Extensions\PathExtension;
 use App\Views\View;
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Route\Router;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -21,7 +23,7 @@ class ViewServiceProvider extends AbstractServiceProvider
         $container = $this->getContainer();
         $config = $container->get('config');
 
-        $container->share(View::class, function () use ($config) {
+        $container->share(View::class, function () use ($config, $container) {
             $loader = new FilesystemLoader(base_path('views'));
 
             $twig = new Environment($loader, [
@@ -32,6 +34,8 @@ class ViewServiceProvider extends AbstractServiceProvider
             if ($config->get('app.debug')) {
                 $twig->addExtension(new DebugExtension);
             }
+
+            $twig->addExtension(new PathExtension($container->get(Router::class)));
 
             return new View($twig);
         });
